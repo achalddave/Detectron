b70e2b7498b93ed6aeade2e46822b058142c9e55
Achal Dave <achalddave@live.com>
2018-06-03 18:35:51 -0400

    Add infer_simple_rpn that extracts RPN proposals

---
+++ tools/infer_simple_rpn.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python2
+
+# Copyright (c) 2017-present, Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
+"""Perform inference on a single image or all images with a certain extension
+(e.g., .jpg) in a folder.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from collections import defaultdict
+import argparse
+import cv2  # NOQA (Must import before importing caffe2 due to bug in cv2)
+import glob
+import logging
+import os
+import pickle
+import sys
+import time
+
+from caffe2.python import workspace
+
+from core.config import assert_and_infer_cfg
+from core.config import cfg
+from core.config import merge_cfg_from_file
+from utils.io import cache_url
+from utils.timer import Timer
+import core.rpn_generator as rpn_engine
+import core.test_engine as infer_engine
+import datasets.dummy_datasets as dummy_datasets
+import utils.c2 as c2_utils
+import utils.logging
+import utils.vis as vis_utils
+
+c2_utils.import_detectron_ops()
+# OpenCL may be enabled by default in OpenCV3; disable it because it's not
+# thread safe and causes unwanted GPU memory allocations.
+cv2.ocl.setUseOpenCL(False)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='End-to-end inference')
+    parser.add_argument(
+        '--cfg',
+        dest='cfg',
+        help='cfg model file (/path/to/model_config.yaml)',
+        default=None,
+        type=str
+    )
+    parser.add_argument(
+        '--wts',
+        dest='weights',
+        help='weights model file (/path/to/model_weights.pkl)',
+        default=None,
+        type=str
+    )
+    parser.add_argument(
+        '--output-dir',
+        dest='output_dir',
+        help='directory for visualization pdfs (default: /tmp/infer_simple)',
+        default='/tmp/infer_simple',
+        type=str
+    )
+    parser.add_argument(
+        '--image-ext',
+        dest='image_ext',
+        help='image file name extension (default: jpg)',
+        default='jpg',
+        type=str
+    )
+    parser.add_argument(
+        'im_or_folder', help='image or folder of images', default=None
+    )
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    return parser.parse_args()
+
+
+def main(args):
+    logger = logging.getLogger(__name__)
+    merge_cfg_from_file(args.cfg)
+    cfg.NUM_GPUS = 1
+    cfg.MODEL.MASK_ON = False
+    cfg.MODEL.RPN_ONLY = True
+    args.weights = cache_url(args.weights, cfg.DOWNLOAD_CACHE)
+    assert_and_infer_cfg(cache_urls=False)
+    model = infer_engine.initialize_model_from_cfg(args.weights)
+    dummy_coco_dataset = dummy_datasets.get_coco_dataset()
+
+    if os.path.isdir(args.im_or_folder):
+        im_list = glob.iglob(args.im_or_folder + '/*.' + args.image_ext)
+    else:
+        im_list = [args.im_or_folder]
+
+    for i, im_name in enumerate(im_list):
+        os.makedirs(args.output_dir, exist_ok=True)
+        base_name = os.path.splitext(os.path.basename(im_name))[0]
+        out_image = os.path.join(
+            args.output_dir, '{}'.format(base_name + '_rpn.png')
+        )
+        out_data = os.path.join(
+            args.output_dir, '{}'.format(base_name + '_rpn.pickle')
+        )
+
+        if os.path.isfile(out_image) and os.path.isfile(out_data):
+            # logger.info('Already processed {}, skipping'.format(im_name))
+            continue
+        else:
+            logger.info('Processing {} -> {}'.format(im_name, out_image))
+        im = cv2.imread(im_name)
+        timers = defaultdict(Timer)
+        t = time.time()
+        with c2_utils.NamedCudaScope(0):
+            # rpn_boxes shape is (num_boxes, 4), rpn_scores is (num_boxes,).
+            rpn_boxes, rpn_scores = rpn_engine.im_proposals(model, im)
+        # Merge rpn_scores into rpn_boes, so that rpn_boxes will be of shape
+        # (num_boxes, 5)
+        import numpy as np
+        rpn_boxes = np.hstack((rpn_boxes, np.expand_dims(rpn_scores, 1)))
+        logger.info('Inference time: {:.3f}s'.format(time.time() - t))
+        for k, v in timers.items():
+            logger.info(' | {}: {:.3f}s'.format(k, v.average_time))
+        if i == 0:
+            logger.info(
+                ' \ Note: inference on the first image will be slower than the '
+                'rest (caches and auto-tuning need to warm up)'
+            )
+
+        if not os.path.isfile(out_data):
+            with open(out_data, 'wb') as f:
+                pickle.dump({
+                    'rpn_boxes': rpn_boxes
+                }, f)
+
+        if not os.path.isfile(out_image):
+            logging.info('Visualizing %s', out_image)
+            vis_utils.vis_one_image(
+                im[:, :, ::-1],  # BGR -> RGB for visualization
+                base_name,
+                args.output_dir,
+                [rpn_boxes],
+                # dataset=dummy_coco_dataset,
+                box_alpha=0.3,
+                show_class=True,
+                thresh=0.7,
+                kp_thresh=2,
+                dpi=300,
+                ext='png'
+            )
+
+
+if __name__ == '__main__':
+    workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
+    utils.logging.setup_logging(__name__)
+    args = parse_args()
+    main(args)
